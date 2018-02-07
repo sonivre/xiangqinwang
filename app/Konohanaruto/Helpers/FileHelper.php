b0e72971ad279fe1d1983884ca668b4037b7449f
Konohanaruto <kingshi2010@gmail.com>
2018-02-07 13:02:16 +0000

    修复了字符串长度过长导致正则匹配失败的bug

--- app/Konohanaruto/Helpers/FileHelper.php
+++ app/Konohanaruto/Helpers/FileHelper.php
@@ -18,13 +18,27 @@ class FileHelper
      *
      * @param $baseString
      * @return mixed
+     *
+     * @bugs
+     * @link https://stackoverflow.com/questions/7620910/regexp-in-preg-match-function-returning-browser-error
+     * description:
+     * 图片的base64字符串过大，导致正则匹配失败
+     *
+     * Stacksize   pcre.recursion_limit
+     *   64 MB      134217
+     *   32 MB      67108
+     *   16 MB      33554
+     *   8 MB       16777
+     *   4 MB       8388
+     *   2 MB       4194
+     *   1 MB       2097
+     *   512 KB     1048
+     *   256 KB     524
      */
     public function detectImageExtensionFromBase64($baseString)
     {
-        $reg = '/image\/(.*);base64,/';
-
-        if (preg_match($reg, $baseString, $match) && ! empty($match[1])) {
-            return $match[1];
+        if ($firstPos = strpos($baseString, ';base64,')) {
+            return str_replace('data:image/', '', substr($baseString, 0, $firstPos));
         }
 
         // 没有文件识别头的base64字符串
